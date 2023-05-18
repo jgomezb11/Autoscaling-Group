c762ac50020198b0a5e4930c454bc158bbf4c957
juan9572 <jprinconu@eafit.edu.co>
2023-05-17 19:37:30 -0500

    Change logic for writing in db

--- monitorS.py
+++ monitorS.py
@@ -1,18 +1,16 @@
 import grpc
-from concurrent import futures
 from monitorC_pb2 import Request
 from monitorC_pb2_grpc import MonitorCStub
 import time
+import datetime
 import threading
 from pymongo import MongoClient
 
 average_memory = 0
 
-MONITORC_HOSTS = []
-
 def get_status(host, port):
     status = False
-    for i in range(0,3):
+    for _ in range(0, 4):
         try:
             channel = grpc.insecure_channel(f'{host}:{port}')
             monitorc_stub = MonitorCStub(channel)
@@ -30,72 +28,76 @@ def get_memory_usage(host, port):
     memory_usage_response = monitorc_stub.GetMemoryUsage(Request(code=0))
     return memory_usage_response.usage
 
-def update_from_database():
-    global MONITORC_HOSTS
-    while True:
-        current_hosts = []
-        col = collection.find_one()
-        if len(col['hosts']) != 0:
-            for i in range(len(col["hosts"])):
-                if col["status"][i]["state"]:
-                    current_hosts.append(col["hosts"][i]["ip"])
-            MONITORC_HOSTS = current_hosts
-
 def get_status_loop():
     while True:
         index = 0
-        for monitorc_host in collection.find_one()['hosts']:
-            host = monitorc_host['ip']
-            status = get_status(host, "50051")
+        with db_lock:
             configuration = collection.find_one()
-            configuration["status"][index]["state"] = status
-            collection.update_one({'_id': configuration['_id']}, {'$set': configuration})
-            index += 1
-        time.sleep(2)
+        hosts = configuration["hosts"]
+        while index < len(hosts):
+            try:
+                host = hosts[index]['ip']
+                status = get_status(host, "50051")
+                if status:
+                    with db_lock:
+                        configuration = collection.find_one()
+                        configuration["status"][index]["isReady"] = True
+                        configuration["status"][index]["state"] = status
+                        collection.update_one({'_id': configuration['_id']}, {'$set': {'status': configuration["status"]}})
+                elif configuration["status"][index]["isReady"]:
+                    with db_lock:
+                        configuration = collection.find_one()
+                        hosts_filtrados = [host for host in configuration["hosts"] if host['id_instance'] != configuration["hosts"][index]["id_instance"]]
+                        status_filtrados = [host for host in configuration["status"] if host['id_instance'] != configuration["status"][index]["id_instance"]]
+                        configuration['hosts'] = hosts_filtrados
+                        configuration['status'] = status_filtrados
+                        collection.update_one({'_id': configuration['_id']}, {'$set': {'hosts': configuration['hosts'], 'status': configuration['status']}})
+                    index -= 1
+                index += 1
+            except:
+                continue
+        time.sleep(4)
 
 def get_average_memory_usage():
     while True:
         total_memory_usage = 0
-        num_hosts = len(MONITORC_HOSTS)
-        if num_hosts != 0:
+        with db_lock:
             configuration = collection.find_one()
+        num_hosts = len(configuration["hosts"])
+        if num_hosts != 0:
             index = 0
-            for monitorc_host in MONITORC_HOSTS:
-                host = monitorc_host
-                memory_usage = get_memory_usage(host, "50051")
-                configuration["status"][index]["memory_usage"] = memory_usage
-                total_memory_usage += memory_usage
-                index += 1
-
+            hosts = configuration["hosts"]
+            while index < len(hosts):
+                try:
+                    host = hosts[index]["ip"]
+                    if get_status(host, "50051"):
+                        memory_usage = get_memory_usage(host, "50051")
+                        time_stap = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                        with db_lock:
+                            configuration = collection.find_one()
+                            configuration["status"][index]["memory_usage"].append(memory_usage)
+                            configuration["status"][index]["time_stap"].append(time_stap)
+                            collection.update_one({'_id': configuration['_id']}, {'$set': {'status': configuration["status"]}})
+                        total_memory_usage += memory_usage
+                    index += 1
+                except:
+                    continue
             average_memory_usage = total_memory_usage // num_hosts
-            configuration["average_memory"] = average_memory_usage
-            collection.update_one({'_id': configuration['_id']}, {'$set': configuration})
-            time.sleep(5)
+            with db_lock:
+                configuration = collection.find_one()
+                collection.update_one({'_id': configuration['_id']}, {'$set': {'average_memory': average_memory_usage}})
+        time.sleep(7)
 
 if __name__ == '__main__':
-    global client, database, collection
+    global client, database, collection, db_lock
     client = MongoClient("mongodb://localhost:27017")
     database = client["ASG"]
     collection = database["config"]
+    db_lock = threading.Lock()
     memory_thread = threading.Thread(target=get_average_memory_usage, daemon=True)
     status_thread = threading.Thread(target=get_status_loop, daemon=True)
-    updates = threading.Thread(target=update_from_database, daemon=True)
-    updates.start()
-    time.sleep(1)
-    memory_thread.start()
     status_thread.start()
-    memory_thread.join()
+    memory_thread.start()
     status_thread.join()
-    updates.join()
+    memory_thread.join()
     client.close()
-
-'''{
-    hosts: [{id_instance:str, ip:str}],
-    average_memory: int,
-    status: [{id_instance:str,state:bool}],
-    min_instances: int,
-    max_instances: int,
-    cpu_threshold: int,
-    scale_up_factor: int,
-    scale_down_factor: double
-}'''
